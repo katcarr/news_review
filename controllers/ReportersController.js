1391d4da9a8fccb972a09868369fa651300071e9
katcarr <kathrynelizabethcarr@gmail.com>
2015-02-27 12:05:12 -0800

    Add local function in Reporters controller to be called when adding a reporter to return to home after addReporter funtion is called

--- controllers/ReportersController.js
+++ controllers/ReportersController.js
@@ -1,5 +1,12 @@
-newspaper.controller( 'ReportersCtrl', function ReportersCtrl($scope, $stateParams, BeatsFactory) {
+newspaper.controller( 'ReportersCtrl', function ReportersCtrl($scope, $stateParams, $state, BeatsFactory) {
   $scope.BeatsFactory = BeatsFactory;
   $scope.beat = BeatsFactory.beats[parseInt($stateParams.beatId) - 1];
+
+  $scope.addReporterandGoBack = function(){
+
+    BeatsFactory.addReporter();
+    $state.go("home");
+
+  }
 
 });
