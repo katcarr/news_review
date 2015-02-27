e1a94e76e728d2fc58f33f527ca124571e8224b7
katcarr <kathrynelizabethcarr@gmail.com>
2015-02-27 12:00:35 -0800

    Add reporter form and addReporter function

--- services/BeatsFactory.js
+++ services/BeatsFactory.js
@@ -1,13 +1,21 @@
-newspaper.factory('BeatsFactory', function BeatsFactory() {
+newspaper.factory('BeatsFactory', function BeatsFactory($stateParams) {
   var factory = {};
   var reporterForm = true;
 
   factory.beats = [];
 
   factory.addBeat = function(){
-    var beat = {name: factory.beatName, id: factory.beats.length + 1};
+    var beat = {name: factory.beatName, id: factory.beats.length + 1, reporters: []};
     factory.beats.push(beat);
     factory.beatName = null;
+  };
+
+  factory.addReporter = function(){
+    var reporter = {name: factory.reporterName, employmentYears: factory.employmentYears, birthdate: factory.birthdate}
+    factory.beats[parseInt($stateParams.beatId) -1].reporters.push(reporter);
+    factory.reporterName = null;
+    factory.employmentYears = null;
+    factory.birthdate = null;
   };
 
   return factory;
