424b33b04348d73158877ffc42667b787a3f06e0
katcarr <kathrynelizabethcarr@gmail.com>
2015-02-27 10:09:02 -0800

    Add form and function to add a beat

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 var newspaper = angular.module("newspaper", ['ui.router']);
+
+
+newspaper.config(function($stateProvider){
+  $stateProvider.state('home', {
+    url: "",
+    templateUrl: "partials/home.html",
+    controller: "BeatsCtrl"
+  });
+
+  $stateProvider.state('beats', {
+    url: "/beats.html",
+    templateUrl: "partials/beats.html",
+    controller: "BeatsCtrl"
+  });
+});
--- services/BeatsFactory.js
+++ services/BeatsFactory.js
@@ -1,7 +1,14 @@
 newspaper.factory('BeatsFactory', function BeatsFactory() {
   var factory = {};
 
+  factory.beats = [];
 
+  factory.addBeat = function(){
+    var beat = {name: factory.beatName};
+    factory.beats.push(beat);
+    factory.beatName = null;
+
+  };
 
   return factory;
 });
